ed83e331f5fe0028b6dce53a720f9e5baa3beac1
philipsoitu <95496892+philipsoitu@users.noreply.github.com>
2023-03-10 21:42:15 -0500

    yo

--- python solutions/p021.py
+++ python solutions/p021.py
@@ -1,15 +1,18 @@
 def divisors(n):
     divs = []
-    for i in range(1,n+1):
-        if n%i==0:
+    for i in range(1, n):
+        if n % i == 0:
             divs.append(i)
 
     return divs
 
 
+ans = 0
 
-
-for i in range(1,10000):
+for i in range(1, 10000):
     a = sum(divisors(i))
-    if i==sum(divisors(a)) and a!=i:
-        print(i)
+    if i == sum(divisors(a)) and a != i:
+        ans += i
+
+
+print(ans)
