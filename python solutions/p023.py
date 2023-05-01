921a7944924a99a34cf4d08518442941faa09aac
philipsoitu <95496892+philipsoitu@users.noreply.github.com>
2023-05-01 10:21:30 -0400

    yo

--- python solutions/p023.py
+++ python solutions/p023.py
@@ -6,20 +6,38 @@ def sumFactors(n):
             fact.add(i)
     return sum(fact)
 
-print(sumFactors(10))
 
+abundant_numbers = set()
 
-
-for i in range(100):
+for i in range(28124):
 
     if sumFactors(i)>i:
         # abundant number
-        print(f"{i} is abundant")
-    elif sumFactors(i)<i:
-        # deficient number
-        print(f"{i} is deficient")
-    else: 
-        # perfect number
-        print(f"{i} is perfect")
-
+        abundant_numbers.add(i)
         
+print("abundant numbers found")
+
+sum_of_abundant = set()
+for num1 in abundant_numbers:
+    for num2 in abundant_numbers:
+        sum_of_abundant.add(num1+num2)
+
+print("found sum of abundant numbers")
+
+excluding_big_nums = set()
+
+for num in sum_of_abundant:
+    if num<28124:
+        excluding_big_nums.add(num)
+
+all_nums = set()
+
+for i in range(28124):
+    all_nums.add(i)
+
+print("created set of all numbers until 28124")
+
+ans_set = all_nums ^ excluding_big_nums
+
+
+print(sum(ans_set))
